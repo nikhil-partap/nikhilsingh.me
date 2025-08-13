6c815b6dcfb373587e5866da0e8dd5dd2647e122
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-08-13 10:43:28 +0530

    Add gh-pages dependency and update deployment script in package.json; set base path in vite.config.js for correct asset loading

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nikhilsingh.me/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
